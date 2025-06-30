a529b4d1b8e1d634de1fc0db8949d64323ada094
Jango <rafaeljangodev@gmail.com>
2025-06-30 00:16:54 -0300

    Visual: Home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,10 @@
-import { HomeHeader } from "@/components/HomeHeader";
 import { View } from "react-native";
+import { router } from "expo-router";
+
+import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
+import { List } from "@/components/List";
+import { Button } from "@/components/Button";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -7,10 +12,51 @@ const summary = {
   output: { label: "Saídas", value: "-R$ 883.90" },
 };
 
+const targets = [
+  {
+    id: "1",
+    name: "Apple Watch",
+    percentage: "50%",
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+  },
+  {
+    id: "2",
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 1.200,00",
+  },
+  {
+    id: "3",
+    name: "Fazer uma viagem para o Rio de Janeiro",
+    percentage: "75%",
+    current: " R$ 1.200,00",
+    target: " R$ 3.000,00",
+  },
+];
+
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
+
+      <List
+        title="Metas"
+        data={targets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Target
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+            data={item}
+          />
+        )}
+        emptyMessage="Nenhuma meta. Toque em nova meta para criar"
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
---
+++ src/components/List/index.tsx
@@ -0,0 +1,44 @@
+import {
+  FlatList,
+  FlatListProps,
+  Text,
+  View,
+  ViewStyle,
+  StyleProp,
+} from "react-native";
+
+import { styles } from "./style";
+import { colors } from "@/theme";
+import { Separator } from "../Separator";
+
+type Props<T> = FlatListProps<T> & {
+  title: string;
+  emptyMessage?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+};
+
+export function List<T>({
+  title,
+  emptyMessage,
+  containerStyle,
+  data,
+  renderItem,
+  ...rest
+}: Props<T>) {
+  return (
+    <View style={[styles.container, containerStyle]}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        ItemSeparatorComponent={() => <Separator color={colors.gray[200]} />}
+        contentContainerStyle={styles.listContent}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.empty}>{emptyMessage}</Text>
+        )}
+        {...rest}
+      />
+    </View>
+  );
+}
