4238e76ce9ca1bf92e21c552dfbc22f469733c67
Jango <rafaeljangodev@gmail.com>
2025-07-04 13:08:15 -0300

    Database criada

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Stack } from "expo-router";
 import { colors } from "@/theme/colors";
 import {
@@ -6,6 +7,9 @@ import {
   Inter_500Medium,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
+
+import { SQLiteProvider } from "expo-sqlite";
+import { migrate } from "@/database/migrate";
 
 import Loading from "@/components/Loading";
 
@@ -19,12 +23,17 @@ export default function Layout() {
   if (!fontsLoaded) {
     return <Loading />;
   }
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   );
 }
