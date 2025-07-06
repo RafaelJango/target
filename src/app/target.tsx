3f9d71cdb563d5fda82cb2a0b4058b7b91f99b19
Jango <rafaeljangodev@gmail.com>
2025-07-06 20:51:31 -0300

    Configurando Database

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,10 +1,51 @@
-import { View } from "react-native";
+import { useState } from "react";
+import { Alert, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+
 import { PageHeader } from "@/components/Page Header";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 
+import { useTargetDatabase } from "@/database/useTargetDatabase";
+
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [name, setName] = useState("");
+  const [amount, setAmout] = useState(0);
+
+  const params = useLocalSearchParams<{ id?: string }>();
+  const targetDatabase = useTargetDatabase();
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert("Atenção", "Preencha nome e valor.");
+    }
+    setIsProcessing(true);
+
+    if (params.id) {
+      // update
+    } else {
+      create();
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({ name, amount });
+      await Alert.alert("Nova Meta", "Meta criada com sucesso!", [
+        {
+          text: "OK",
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível criar a meta.");
+      console.log(error);
+      setIsProcessing(false);
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
@@ -15,9 +56,19 @@ export default function Target() {
         <Input
           label="Nome da meta"
           placeholder="Ex: Viagem para praia, Apple Watch"
+          onChangeText={setName}
+          value={name}
         />
-        <CurrencyInput label="Valor alvo (R$)" value={0} />
-        <Button title="Salvar" />
+        <CurrencyInput
+          label="Valor alvo (R$)"
+          value={amount}
+          onChangeValue={setAmout}
+        />
+        <Button
+          title="Salvar"
+          onPress={handleSave}
+          isProcessing={isProcessing}
+        />
       </View>
     </View>
   );
